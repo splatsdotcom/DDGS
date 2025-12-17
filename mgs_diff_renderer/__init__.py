a53a61595cba6899955b91c40ad3aefe85691462
Daniel Elwell <elwell.daniel.2@gmail.com>
2025-12-17 18:05:05 +0000

    added from_colmap method for settings

--- mgs_diff_renderer/__init__.py
+++ mgs_diff_renderer/__init__.py
@@ -1,7 +1,41 @@
 import torch
 import torch.nn as nn
+import math
 from . import _C
 
+# ------------------------------------------- #
+
+def _look(eye, forward, up):
+	f = forward / torch.norm(forward)
+	u = up / torch.norm(up)
+
+	s = torch.cross(f, u, dim=0)
+	s = s / torch.norm(s)
+	
+	u = torch.cross(s, f, dim=0)
+	
+	m = torch.eye(4, dtype=torch.float32, device=eye.device)
+	m[0, :3] = s
+	m[1, :3] = u
+	m[2, :3] = -f
+	m[0, 3] = -torch.dot(s, eye)
+	m[1, 3] = -torch.dot(u, eye)
+	m[2, 3] = torch.dot(f, eye)
+
+	return m
+
+def _perspective(fovY, aspect, zNear, zFar):
+	tanHalfFov = math.tan(fovY / 2)
+
+	m = torch.zeros((4, 4), dtype=torch.float32)
+	m[0, 0] = 1 / (aspect * tanHalfFov)
+	m[1, 1] = 1 / tanHalfFov
+	m[2, 2] = -(zFar + zNear) / (zFar - zNear)
+	m[2, 3] = -(2 * zFar * zNear) / (zFar - zNear)
+	m[3, 2] = -1.0
+	
+	return m
+	
 # ------------------------------------------- #
 
 # just a wrapper for torch.classes.mgs_diff_renderer.Settings
@@ -17,6 +51,43 @@ class Settings:
 			proj,
 			focalX,
 			focalY,
+			debug
+		)
+
+	@staticmethod
+	def from_colmap(width: int, height: int, focalX: float, focalY: float, R: torch.Tensor, T: torch.Tensor,
+					debug: bool = False):
+		
+		# flip sign convention:
+		# ---------------
+		R = -R
+
+		# get view matrix
+		# ---------------
+		camPos = -R.T @ T
+
+		forward = R[2, :3]
+		up = R[1, :3]
+		
+		view = _look(camPos, forward, up)
+
+		# get proj matrix:
+		# ---------------
+		aspect = width / height
+		fovY = 2 * math.atan(height / (2 * focalY))
+		zNear = 0.1
+		zFar = 1000.0
+		
+		proj = _perspective(fovY, aspect, zNear, zFar).to(R.device)
+
+		focalX = width / (2 * math.tan(fovY / 2))
+		focalY = focalX
+
+		# return settings:
+		# ---------------
+		return Settings(
+			width, height, 
+			view, proj, focalX, focalY,
 			debug
 		)
 
